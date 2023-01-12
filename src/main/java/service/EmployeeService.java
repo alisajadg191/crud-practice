a40845d3d776859a1bb587365b3dbbd8cbbc8dbf
sajad <alisajadg191@gmail.com>
2023-01-12 17:26:38 +0000

    Initial commit

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
